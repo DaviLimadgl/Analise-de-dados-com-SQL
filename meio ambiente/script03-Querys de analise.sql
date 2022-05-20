72b9bc5ea71c81c1e779fa4459449b16cc9c14a8
Davi <mondromodg@hotmail.com>
2022-05-20 20:42:16 -0300

    atualização

--- meio ambiente/script03-Querys de analise.sql
+++ meio ambiente/script03-Querys de analise.sql
@@ -1,13 +1,21 @@
 select * from cap09.tb_global_qualidade_ar;
 
+
+
 # 1- Quais poluentes foram considerados na pesquisa?
 
 select distinct 
 pollutant from 
 cap09.tb_global_qualidade_ar; 
 
+# 2- Total de cidades onde foram coletada amostras:
 
-# 2- Qual foi a média de poluição ao longo do tempo provocada pelo poluente ground-level ozone (o3)?
+select 
+count(distinct city) as numero_de_cidades
+from cap09.tb_global_qualidade_ar
+order by 1;
+
+# 3- Qual foi a média de poluição ao longo do tempo provocada pelo poluente ground-level ozone (o3)?
 
 select 
 country as pais,
@@ -18,7 +26,7 @@ from cap09.tb_global_qualidade_ar
 where pollutant = "o3"
 order by 1,3;
 
-# 3- Qual foi a média de poluição causada pelo poluente ground-level ozone (o3) por país medida em µg/m³ (microgramas por metro cúbico)?
+# 4- Qual foi a média de poluição causada pelo poluente ground-level ozone (o3) por país medida em µg/m³ (microgramas por metro cúbico)?
 
 
 select 
@@ -33,7 +41,7 @@ group by 1,2
 order by 1;
 
 
-# 4- Considerando o resultado anterior, qual país teve maior índice de poluição geral por o3, Itália (IT) ou Espanha (ES)? Por quê?
+# 5- Considerando o resultado anterior, qual país teve maior índice de poluição geral por o3, Itália (IT) ou Espanha (ES)? Por quê?
 
 select 
 country as pais,
@@ -51,6 +59,17 @@ and country in('IT', 'ES')
 group by 1,2
 order by 1;
 
-# 5- Quais localidades e países tiveram média de poluição em 2020 superior a 100 µg/m³ para o poluente fine particulate matter (pm25)?
+# 6- Quais localidades e países tiveram média de poluição em 2020 superior a 100 µg/m³ para o poluente fine particulate matter (pm25)?
 
-
+select 
+coalesce(location, "Total") as localidade,
+coalesce(country, "Total") as pais,
+cast(timestamp as date) as data_coleta,
+round(avg(value),2) as media
+from cap09.tb_global_qualidade_ar
+where unit = "µg/m³"
+and pollutant = "pm25"
+and year(timestamp) = 2020
+group by 1,2 with rollup
+having media > 100
+order by 1,2;
